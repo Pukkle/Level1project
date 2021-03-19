66b3d39112b873857eea4e56eb44b9581bfde55e
pukkle_ <timmsr@stu.otc.school.nz>
2021-03-19 13:17:06 +1300

    A weird mix of sports and gaming

--- level1project.py
+++ level1project.py
@@ -13,7 +13,7 @@ print("On the fifth game they scored {} points".format(fifth_match))
 print("In total they scored {} points".format(total_points))
 print("")
 
-#Gift Card
+#Gift Cards
 gift_card = 50
 skin = 10
 weapon = 10
