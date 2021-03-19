19167c0c4ee217b2f710d90a350c33fc6cfb9743
pukkle_ <timmsr@stu.otc.school.nz>
2021-03-19 13:15:24 +1300

    A weird mix of sports and gaming

--- level1project.py
+++ level1project.py
@@ -1,1 +1,24 @@
-#Coment
+#Points from team
+first_match = 3
+second_match = 6
+third_match = 10
+forth_match = 10
+fifth_match = 6
+total_points = first_match + second_match + third_match + forth_match + fifth_match
+print("On the first game they scored {} points".format(first_match))
+print("On the second game they scored {} points".format(second_match))
+print("On the third game they scored {} points".format(third_match))
+print("On the forth game they scored {} points".format(forth_match))
+print("On the fifth game they scored {} points".format(fifth_match))
+print("In total they scored {} points".format(total_points))
+print("")
+
+#Gift Card
+gift_card = 50
+skin = 10
+weapon = 10
+gems = 15
+print("Reuben has {} on his gift card".format(gift_card))
+gift_card = gift_card - skin - weapon - gems
+print("Reuben has spent {} on a skin, {} on a new weapon, and {} on gems".format(skin, weapon, gems))
+print("Reuben now has {} on his gift card".format(gift_card))
